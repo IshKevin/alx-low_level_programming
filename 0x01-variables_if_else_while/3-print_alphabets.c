eeaed069600fc2fd33e93e75ef6ee46eff0834bf
IshKevin <carterk279@gmail.com>
2023-02-18 13:15:30 -0800

    fix error

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,16 +9,16 @@ int main(void)
 	char lcase = 'a';
 	char ucase = 'A';
 
-	while (lcase <= 'z') 
+	while (lcase <= 'z') /* runs each lowercase letter a, b, c, ...z*/
 	{
-		putchar(lcase); 
-		lcase++;  
+		putchar(lcase); /*output a char to stdout */
+		lcase++;  /*keeps incrementing by one char until it reaches 'Z'*/
 	}
-	while (ucase <= 'Z')
+	while (ucase <= 'Z') /* runs each lowercase letter a, b, c, ...z*/
 	{
-		putchar(ucase); 
-		ucase++; 
+		putchar(ucase); /*prints a char to stdout */
+		ucase++; /*keeps incrementing by one char until it reaches 'Z'*/
 	}
-	putchar('\n');
+	putchar('\n'); /*add newline at end*/
 	return (0);
 }
